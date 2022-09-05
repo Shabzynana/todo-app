b844d17399e69024a4245783681babe611c7099b
Adebiyi Oluwafemi <horlarmihleykan10@gmail.com>
2022-09-05 22:10:33 +0100

    ss

--- todo_app/__init__.py
+++ todo_app/__init__.py
@@ -19,7 +19,6 @@ app.config['SECRET_KEY'] = 'mysegcret'
 ##########################################
 # basedir = os.path.abspath(os.path.dirname(__file__))
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://shabzy:1111@localhost:5432/flask_app"
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -36,19 +35,12 @@ login_manager.init_app(app)
 login_manager.login_view = 'users.login'
 
 
-# app.config['MAIL_SERVER']='smtp.mailtrap.io'
-# app.config['MAIL_PORT'] = 2525
-# app.config['MAIL_USERNAME'] = '37856c83ca7adc'
-# app.config['MAIL_PASSWORD'] = 'bcaae18b457ba8'
-# app.config['MAIL_USE_TLS'] = True
-# app.config['MAIL_USE_SSL'] = False
-
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['TESTING'] = False
 app.config['MAIL_USERNAME'] = 'shabzynana@gmail.com'
-app.config['MAIL_PASSWORD'] = 'mgtlzuarolxwtuhk'
+app.config['MAIL_PASSWORD'] = ''
 
 
 mail = Mail(app)
